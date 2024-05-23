ef0d5865831a4aaf32d4536939a5ad74bdce3d02
Karl-Xavier <uwasbruno256@gmail.com>
2024-05-23 14:02:51 +0100

    First Commit

---
+++ src/components/RecipeResults/RecipeResults.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const RecipeResults = ({ data }) => {
+    if(!data || !data.results || data.results.length === 0){
+        return (<p style={{textAlign: "center", margin: "10px 0 10px 0", fontWeight: "bold"}}>Nothing Here</p>);
+    }
+
+  return (
+    <div className="container">
+        <p style={{textAlign: "center", margin: "10px 0 10px 0", fontWeight: "bold", color: "blue", fontStyle: "italic"}}>Showing {data.results.length} results</p>
+        <div className="row">
+            {data.results.map((recipe) => {
+                return( 
+                        <div key={recipe.id} className="col-lg-4 col-md-2 col-sm-6 text-center">
+                            <img className="img-fluid rounded" src={recipe.image} alt={recipe.title}/>
+                            <h2>{recipe.title}</h2>
+                        </div>
+                );
+            })}
+        </div>
+    </div>
+  )
+}
+
+export default RecipeResults
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import './SearchBar.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { MagnifyingGlass } from 'phosphor-react'
+
+const SearchBar = (props) => {
+    const [query, setQuery] = useState('');
+    const apiKey = "4a611089e80c42d697a66db9617b8988";
+
+    const handleSearch = async () => {
+        const apiURL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${query}`;
+        try {
+            const response = await fetch(apiURL, {
+                method: "GET",
+                headers: {
+                    "Content-Type": 'application/json'
+                },
+            });
+            const data = await response.json();
+            props.onSearch(data)
+            console.log(data);
+        } catch (error) {
+            console.log("Error Fetching Data...", error);
+        }
+
+    };
+
+    return (
+        <div className='searchbar container'>
+            <input
+                type='text'
+                value={query}
+                onChange={(e) => { setQuery(e.target.value) }}
+                placeholder='Search Food'
+            />
+            <MagnifyingGlass size={40} role='button' onClick={handleSearch}/>
+        </div>
+    );
+};
+
+export default SearchBar;
